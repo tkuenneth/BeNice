c612dc48ad03aa65ff9da8a8d017a8e5c9f89a5f
Thomas Künneth <kuennetht@gmail.com>
2023-12-28 12:27:01 +0100

    bottom sheet added

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -9,7 +9,7 @@ android {
 
     defaultConfig {
         applicationId = "de.thomaskuenneth.benice"
-        minSdk = 30
+        minSdk = 26
         targetSdk = 34
         versionCode = 1
         versionName = "1.0"
--- app/src/main/java/de/thomaskuenneth/benice/BeNiceActivity.kt
+++ app/src/main/java/de/thomaskuenneth/benice/BeNiceActivity.kt
@@ -12,13 +12,18 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.annotation.DrawableRes
+import androidx.annotation.StringRes
+import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.combinedClickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
@@ -26,12 +31,23 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.statusBarsPadding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.ModalBottomSheet
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.ColorFilter
 import androidx.compose.ui.graphics.asImageBitmap
+import androidx.compose.ui.hapticfeedback.HapticFeedbackType
+import androidx.compose.ui.platform.LocalHapticFeedback
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 
 
@@ -88,8 +104,11 @@ class AppPickerActivity : ComponentActivity() {
     }
 }
 
+@OptIn(ExperimentalFoundationApi::class, ExperimentalMaterial3Api::class)
 @Composable
 fun TwoApps(appInfoList: List<AppInfo>, onClick: (AppInfo) -> Unit) {
+    var contextMenuAppInfo by rememberSaveable { mutableStateOf<AppInfo?>(null) }
+    val haptics = LocalHapticFeedback.current
     LazyColumn(
         modifier = Modifier
             .fillMaxSize()
@@ -100,9 +119,14 @@ fun TwoApps(appInfoList: List<AppInfo>, onClick: (AppInfo) -> Unit) {
                 Row(
                     modifier = Modifier
                         .fillMaxWidth()
-                        .clickable {
-                            onClick(appInfo)
-                        }
+                        .combinedClickable(
+                            onClick = { onClick(appInfo) },
+                            onLongClick = {
+                                haptics.performHapticFeedback(HapticFeedbackType.LongPress)
+                                contextMenuAppInfo = appInfo
+                            },
+                            onLongClickLabel = stringResource(id = R.string.open_context_menu)
+                        )
                         .padding(all = 16.dp),
                     horizontalArrangement = Arrangement.Start,
                     verticalAlignment = Alignment.CenterVertically
@@ -115,6 +139,23 @@ fun TwoApps(appInfoList: List<AppInfo>, onClick: (AppInfo) -> Unit) {
                     )
                 }
             }
+        }
+    }
+    if (contextMenuAppInfo != null) {
+        ModalBottomSheet(
+            onDismissRequest = { contextMenuAppInfo = null },
+            windowInsets = WindowInsets(0),
+        ) {
+            MenuItem(
+                onClick = {
+                    contextMenuAppInfo?.let {
+                        onClick(it)
+                    }
+                    contextMenuAppInfo = null
+                },
+                imageRes = R.drawable.baseline_launch_24,
+                textRes = R.string.launch
+            )
         }
     }
 }
@@ -134,7 +175,35 @@ fun AppIconImage(drawable: Drawable) {
     ) {
         Image(
             bitmap = bitmap.asImageBitmap(),
-            "An image"
+            contentDescription = null
+        )
+    }
+}
+
+@Composable
+fun MenuItem(
+    onClick: () -> Unit,
+    @DrawableRes imageRes: Int,
+    @StringRes textRes: Int
+) {
+    Row(
+        verticalAlignment = Alignment.CenterVertically,
+        modifier = Modifier
+            .fillMaxWidth()
+            .clickable {
+                onClick()
+            }
+            .padding(all = 16.dp)
+    ) {
+        Image(
+            painter = painterResource(id = imageRes),
+            contentDescription = null,
+            colorFilter = ColorFilter.tint(color = MaterialTheme.colorScheme.primary)
+        )
+        Spacer(modifier = Modifier.width(16.dp))
+        Text(
+            text = stringResource(id = textRes),
+            color = MaterialTheme.colorScheme.primary
         )
     }
 }
