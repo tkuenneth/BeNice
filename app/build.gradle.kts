aa3d834d33e9a1f78161a5d283b2ce3651d09b14
Thomas Künneth <kuennetht@gmail.com>
2024-01-05 17:18:27 +0100

    Library and tools upgrades

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,7 +11,7 @@ android {
         applicationId = "de.thomaskuenneth.benice"
         minSdk = 26
         targetSdk = 34
-        versionCode = 5
+        versionCode = 6
         versionName = "1.0.4"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
