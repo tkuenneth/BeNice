faa5ffbd1fdb6e823868c9e3094bd0ac044ab9b0
Thomas Künneth <kuennetht@gmail.com>
2024-04-28 11:01:58 +0200

    add a quick settings tile

---
+++ app/src/main/java/de/thomaskuenneth/benice/BeNiceTileService.kt
@@ -0,0 +1,35 @@
+package de.thomaskuenneth.benice
+
+import android.app.PendingIntent
+import android.app.TaskStackBuilder
+import android.content.Intent
+import android.os.Build
+import android.service.quicksettings.Tile
+import android.service.quicksettings.TileService
+
+class BeNiceTileService : TileService() {
+
+    override fun onStartListening() {
+        super.onStartListening()
+        qsTile.run {
+            state = Tile.STATE_INACTIVE
+            this.updateTile()
+        }
+    }
+
+    override fun onClick() {
+        val resultIntent = Intent(this, AppChooserActivity::class.java)
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.UPSIDE_DOWN_CAKE) {
+            val resultPendingIntent: PendingIntent = TaskStackBuilder.create(this).run {
+                addNextIntentWithParentStack(resultIntent)
+                getPendingIntent(
+                    0,
+                    PendingIntent.FLAG_CANCEL_CURRENT or PendingIntent.FLAG_IMMUTABLE
+                )
+            }
+            startActivityAndCollapse(resultPendingIntent)
+        } else {
+            startActivityAndCollapse(resultIntent)
+        }
+    }
+}
