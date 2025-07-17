7bea5aba500feb4dea3617791a57137eacf3808a
Thomas Künneth <kuennetht@gmail.com>
2025-07-17 10:51:33 +0200

    Library and tools upgrades

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,7 +12,7 @@ android {
         applicationId = "de.thomaskuenneth.benice"
         minSdk = 28
         targetSdk = 36
-        versionCode = 57
+        versionCode = 58
         versionName = "1.3.5"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
@@ -49,27 +49,27 @@ android {
 }
 
 dependencies {
-    implementation(platform("androidx.compose:compose-bom:2025.06.00"))
+    implementation(platform("androidx.compose:compose-bom:2025.07.00"))
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
-    implementation("androidx.compose.material:material:1.9.0-alpha04")
-    implementation("androidx.compose.material3:material3:1.4.0-alpha15")
+    implementation("androidx.compose.material:material:1.9.0-beta03")
+    implementation("androidx.compose.material3:material3:1.4.0-alpha18")
     implementation("androidx.compose.material:material-icons-extended")
 
-    androidTestImplementation(platform("androidx.compose:compose-bom:2025.06.00"))
+    androidTestImplementation(platform("androidx.compose:compose-bom:2025.07.00"))
     androidTestImplementation("androidx.compose.ui:ui-test-junit4")
     debugImplementation("androidx.compose.ui:ui-test-manifest:")
 
     implementation("androidx.core:core-ktx:1.16.0")
-    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.9.1")
-    implementation("androidx.activity:activity-compose:1.12.0-alpha02")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.9.2")
+    implementation("androidx.activity:activity-compose:1.12.0-alpha04")
     implementation("androidx.appcompat:appcompat:1.7.1")
     implementation("androidx.preference:preference-ktx:1.2.1")
-    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.9.1")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.9.2")
 
-    implementation("androidx.window:window-core:1.5.0-alpha02")
-    implementation("androidx.window:window:1.5.0-alpha02")
+    implementation("androidx.window:window-core:1.5.0-beta01")
+    implementation("androidx.window:window:1.5.0-beta01")
 
     implementation("io.coil-kt:coil-compose:2.7.0")
 
